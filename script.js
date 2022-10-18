17ad41e16d768442d1f6f5aa8419907830b3973b
Allan Viray <allang.v.dev@gmail.com>
2022-10-18 17:57:56 -0500

    Render the game area to the screen

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hellooo!');
+const gameWrapper = document.querySelector('.wrapper');
+
+window.addEventListener('DOMContentLoaded', startGame);
+
+function startGame() {
+  gameArea.start();
+}
+
+// - Ojbect that contains game functionality.
+const gameArea = {
+  canvas: document.createElement('canvas'),
+  start: function () {
+    this.canvas.width = 600;
+    this.canvas.height = 600;
+    this.context = this.canvas.getContext('2d');
+    document.querySelector('.wrapper').appendChild(this.canvas);
+  },
+};
